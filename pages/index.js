f22da959bd9f0a481c861a474068a31b53a5f40e
ammar <ammarraneez@gmail.com>
2022-09-19 10:09:26 +0530

    Add base search UI

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
+import { useRef } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import { MicrophoneIcon, Squares2X2Icon } from '@heroicons/react/24/solid';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import Avatar from '../components/Avatar';
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
   const search = (e) => {
     e.preventDefault();
+    const term = searchInputRef.current.value;
+    if (!term) return;
+    router.push(`/search?term=${term}`);
   };
 
   return (
@@ -17,43 +25,44 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className='flex w-full p-5 justify-between text-sm text-gray-700'>
-        <div className='flex space-x-4 items-center'>
-          <p className='link'>About</p>
-          <p className='link'>Store</p>
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+        <div className="flex space-x-4 items-center">
+          <p className="link">About</p>
+          <p className="link">Store</p>
         </div>
 
-        <div className='flex space-x-4 items-center'>
-          <p className='link'>Gmail</p>
-          <p className='link'>Images</p>
-          <Squares2X2Icon className='h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer' />
-          <Avatar url='/user-image.jpg' />
+        <div className="flex space-x-4 items-center">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
+          <Squares2X2Icon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+          <Avatar url="/user-image.jpg" />
         </div>
       </header>
 
-      <form className='flex flex-col items-center mt-44 flex-grow w-4/5'>
+      <form className="flex flex-col items-center mt-44 flex-grow">
         <Image
           alt="logo"
-          src='/google-logo.png'
+          src="/google-logo.png"
           width={300}
           height={100}
           priority={true}
         />
 
-        <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
-          <MagnifyingGlassIcon className='h-5 mr-3 text-gray-500' />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+          <MagnifyingGlassIcon className="h-5 mr-3 text-gray-500" />
           <input
-            type='text'
-            className='flex-grow focus:outline-none'
+            ref={searchInputRef}
+            type="text"
+            className="flex-grow focus:outline-none"
           />
-          <MicrophoneIcon className='h-5 ' />
+          <MicrophoneIcon className="h-5" />
         </div>
 
-        <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4'>
-          <button onClick={search} className='btn'>
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button onClick={search} className="btn">
             Google Seach
           </button>
-          <button onClick={search} className='btn'>
+          <button onClick={search} className="btn">
             {'I\'m Feeling Lucky'}
           </button>
         </div>
