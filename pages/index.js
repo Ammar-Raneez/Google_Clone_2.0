9d5a03e8479ea82e4b218ffe041d32ca7f220596
ammar <ammarraneez@gmail.com>
2022-09-19 10:06:31 +0530

    Add basic form UI

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Head from 'next/head';
-import { Squares2X2Icon } from '@heroicons/react/24/solid';
+import Image from 'next/image';
+import { MicrophoneIcon, Squares2X2Icon } from '@heroicons/react/24/solid';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import Avatar from '../components/Avatar';
 
 export default function Home() {
+  const search = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <Head>
@@ -24,6 +30,34 @@ export default function Home() {
           <Avatar url='/user-image.jpg' />
         </div>
       </header>
+
+      <form className='flex flex-col items-center mt-44 flex-grow w-4/5'>
+        <Image
+          alt="logo"
+          src='/google-logo.png'
+          width={300}
+          height={100}
+          priority={true}
+        />
+
+        <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
+          <MagnifyingGlassIcon className='h-5 mr-3 text-gray-500' />
+          <input
+            type='text'
+            className='flex-grow focus:outline-none'
+          />
+          <MicrophoneIcon className='h-5 ' />
+        </div>
+
+        <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4'>
+          <button onClick={search} className='btn'>
+            Google Seach
+          </button>
+          <button onClick={search} className='btn'>
+            {'I\'m Feeling Lucky'}
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
